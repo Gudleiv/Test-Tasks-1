c0e54502c08819a7ff903b514e4c630d9ddbc9b5
Gleb Gudleiv <glebgudleiv@gmail.com>
2021-02-27 17:43:40 +0300

    fix vuetify views

--- Task-4/src/App.vue
+++ Task-4/src/App.vue
@@ -25,6 +25,9 @@
         />
       </div>
 
+      <v-btn text to="/">Home</v-btn>
+      <v-btn text :to="{ name: 'About'}">About</v-btn>
+
       <v-spacer></v-spacer>
 
       <v-btn
@@ -38,23 +41,19 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
   }),
 };
 </script>
