e09eab390a6484f94148b678aaecf6cd2843793a
Gleb Gudleiv <glebgudleiv@gmail.com>
2021-02-27 16:41:25 +0300

    add redirect to about

--- Task-3/src/router/index.js
+++ Task-3/src/router/index.js
@@ -13,17 +13,20 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
+  },
+  {
+    path: '/*',
+    redirect: {
+      name: 'About'
     }
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
