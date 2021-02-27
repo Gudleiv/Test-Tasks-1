08253b44cb53c74d367f684d6c10344779050557
Gleb Gudleiv <glebgudleiv@gmail.com>
2021-02-27 13:27:18 +0300

    add auxClick Handler

--- Task-1/index.js
+++ Task-1/index.js
@@ -1,21 +1,27 @@
 const link = document.getElementById('link-yandex')
 const specifiedLink = 'https://google.com'
 const storeName = 'clickCounts'
-let clickCounts = getCounts(storeName)
 
 link.addEventListener('click', clickLink)
+link.addEventListener('auxclick', auxClickLink) //Mouse Wheel Button
 
 function clickLink(event) {
   event.preventDefault()
   const url = event.target.href
-
-  clickCounts++
-  window.localStorage.setItem(storeName, clickCounts)
-  document.cookie = `${storeName} = ${clickCounts}; samesite=strict`
-  console.log(clickCounts)
-
+  setCounts(storeName)
   window.open(url, '_blank', 'noopener,noreferrer')
   window.location.href = specifiedLink
+}
+
+function auxClickLink(event) {
+  if (event.button === 1) setCounts(storeName) // If mouse wheel button clicked
+  window.location.href = specifiedLink
+}
+
+function setCounts(storeName) {
+  const counts = getCounts(storeName) + 1
+  window.localStorage.setItem(storeName, counts)
+  document.cookie = `${storeName} = ${counts}; samesite=strict`
 }
 
 function getCounts(storeName) {
